8f648b39bdba44671f0c386b32d988aaa16b6f18
Henok Kirubel <henokhm2@gmail.com>
2023-08-30 02:47:38 +0300

    Add SQL command to create animals table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
-    name varchar(100)
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg REAL
 );
