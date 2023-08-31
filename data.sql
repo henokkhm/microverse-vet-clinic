258b541eb2bae49f0e31e2c99a72a84b7c34232e
Henok Kirubel <henokhm2@gmail.com>
2023-08-31 23:36:20 +0300

    Insert records into species table

--- data.sql
+++ data.sql
@@ -149,3 +149,13 @@ INSERT INTO
     owners (full_name, age)
 VALUES
     ('Jodie Whittaker', 38);
+
+INSERT INTO
+    species (name)
+VALUES
+    ('Pokemon');
+
+INSERT INTO
+    species (name)
+VALUES
+    ('Digimon');
