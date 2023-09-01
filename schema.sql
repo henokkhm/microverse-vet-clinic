36f9c67ddfa2c832892fab14483daaa62ad0c6b9
Henok Kirubel <henokhm2@gmail.com>
2023-09-01 19:34:30 +0300

    Add specializations association table

--- schema.sql
+++ schema.sql
@@ -39,3 +39,11 @@ CREATE TABLE vets (
     age INT,
     date_of_graduation DATE
 );
+
+CREATE TABLE specializations (
+    vet_id INT,
+    species_id INT,
+    PRIMARY KEY (vet_id, species_id),
+    FOREIGN KEY (vet_id) REFERENCES vets(id),
+    FOREIGN KEY (species_id) REFERENCES species(id)
+);
